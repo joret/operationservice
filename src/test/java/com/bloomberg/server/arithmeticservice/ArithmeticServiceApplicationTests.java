a32186bdd515a29088350f3429bdb5a1147f8f68
marcelo.flores <marcelo.floresman@gmail.com>
2020-04-03 02:01:18 +0100

    commit spring boot clean template

---
+++ src/test/java/com/bloomberg/server/arithmeticservice/ArithmeticServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bloomberg.server.arithmeticservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArithmeticServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
