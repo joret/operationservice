157290020c47a7f2952244eb1a35ffdc9ebfb3da
marcelo.flores <marcelo.floresman@gmail.com>
2020-04-04 02:01:14 +0100

    using operation object  as request body

--- src/main/java/com/bloomberg/server/arithmeticservice/controllers/OperationController.java
+++ src/main/java/com/bloomberg/server/arithmeticservice/controllers/OperationController.java
@@ -1,9 +1,9 @@
 package com.bloomberg.server.arithmeticservice.controllers;
 
-import com.bloomberg.server.arithmeticservice.businesslogic.exceptions.ExpressionSolverException;
 import com.bloomberg.server.arithmeticservice.businesslogic.interfaces.IExpressionSolver;
 import com.bloomberg.server.arithmeticservice.models.Operation;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -23,11 +23,14 @@ public class OperationController {
     public String arithmeticOperation(@RequestBody Operation operation) {
         try{
 
-            //TODO validate expression before using
-            //TODO Class operation seems not to be needed anymore, change with a simple string if thats the case
-            var result = expressionSolver.evaluate(operation.getOperationType());
-            // TODO version API
-            return "{\"result\":" + result + "}";
+
+                //TODO validate expression before using
+                //TODO Class operation seems not to be needed anymore, change with a simple string if thats the case
+                var result = expressionSolver.evaluate(operation.getOperation());
+                // TODO version API
+                return "{\"result\":" + result + "}";
+
+
         } catch (Exception e){
             return "{result: null}";
         }
--- src/main/java/com/bloomberg/server/arithmeticservice/models/Operation.java
+++ src/main/java/com/bloomberg/server/arithmeticservice/models/Operation.java
@@ -1,14 +1,4 @@
 package com.bloomberg.server.arithmeticservice.models;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Data;
-
-import java.util.List;
-
-@Data
 public class Operation {
-
-    @JsonProperty("operation_type")
-    private String operationType;
-    private List<Double> numbers;
 }
