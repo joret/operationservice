c06770bcfca0dbaa4596ec2ef20f896f1250097e
marcelo.flores <marcelo.floresman@gmail.com>
2020-04-06 23:58:33 +0100

    adding expression builder exception

--- src/test/java/com/bloomberg/server/arithmeticservice/businesslogic/ExpressionBuilderTest.java
+++ src/test/java/com/bloomberg/server/arithmeticservice/businesslogic/ExpressionBuilderTest.java
@@ -1,18 +1,39 @@
 package com.bloomberg.server.arithmeticservice.businesslogic;
 
-import com.bloomberg.server.arithmeticservice.configuration.OperationsConfig;
+import com.bloomberg.server.arithmeticservice.businesslogic.exceptions.ExpressionBuildException;
+import com.bloomberg.server.arithmeticservice.businesslogic.exceptions.ExpressionSolverException;
 import com.bloomberg.server.arithmeticservice.models.Expression;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ExpressionBuilderTest {
+    //TODO under development
+
     @Test
-        //TODO under development
-    void build_WhenNullConfig_Return(){
+    void When_NullExpression_Throws_ExpressionException(){
+        var sut = new ExpressionBuilder();
+        assertThrows(ExpressionBuildException.class, () ->sut.build(null));
+    }
+
+    @Test
+    void When_DefaultExpression_Throw_ExpressionBuildException() throws ExpressionBuildException {
         var sut = new ExpressionBuilder();
         var expression = new Expression();
 
-        sut.build(expression, null);
+        assertThrows(ExpressionBuildException.class, () ->sut.build(expression));
+    }
+
+    @Test
+    void When_ValidExpression_Return_StringRepresentationOfExpression() throws ExpressionBuildException {
+        var sut = new ExpressionBuilder();
+        var expression = new Expression();
+        expression.setNumbers(List.of(3.0, 4.0));
+        expression.setOperation('+');
+
+        String result = sut.build(expression);
+        assertEquals("3.0+4.0",result);
     }
 }
