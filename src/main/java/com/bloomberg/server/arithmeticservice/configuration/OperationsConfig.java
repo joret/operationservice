98ef5e0ed55c3bcfb3d0473f33b3ebb11af44263
marcelo.flores <marcelo.floresman@gmail.com>
2020-04-06 04:04:18 +0100

    adding configuration for expressions

--- src/main/java/com/bloomberg/server/arithmeticservice/configuration/OperationsConfig.java
+++ src/main/java/com/bloomberg/server/arithmeticservice/configuration/OperationsConfig.java
@@ -1,4 +1,15 @@
 package com.bloomberg.server.arithmeticservice.configuration;
 
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+
+@Component
+@ConfigurationProperties("operations")
+@Data
 public class OperationsConfig {
+    private Integer maxNumbers = 2;
+    private Set<Character> allowed = Set.of('+');
 }
