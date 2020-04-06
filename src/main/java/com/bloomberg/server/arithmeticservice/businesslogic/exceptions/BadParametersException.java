9b286282fa412540ac8a0208dcc948ffb036b667
marcelo.flores <marcelo.floresman@gmail.com>
2020-04-06 04:03:26 +0100

    binding badrequestexception to 400

--- src/main/java/com/bloomberg/server/arithmeticservice/businesslogic/exceptions/BadParametersException.java
+++ src/main/java/com/bloomberg/server/arithmeticservice/businesslogic/exceptions/BadParametersException.java
@@ -1,4 +1,15 @@
 package com.bloomberg.server.arithmeticservice.businesslogic.exceptions;
 
-public class BadParametersException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class BadParametersException extends Exception{
+    public BadParametersException(){
+
+    }
+
+    public BadParametersException(String message){
+        super(message);
+    }
 }
