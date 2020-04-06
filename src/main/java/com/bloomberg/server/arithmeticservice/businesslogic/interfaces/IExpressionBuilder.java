1dd6e57f875288576833ed6cadfd6e1e5bbd4421
marcelo.flores <marcelo.floresman@gmail.com>
2020-04-06 23:57:42 +0100

    adding expression builder exception

--- src/main/java/com/bloomberg/server/arithmeticservice/businesslogic/interfaces/IExpressionBuilder.java
+++ src/main/java/com/bloomberg/server/arithmeticservice/businesslogic/interfaces/IExpressionBuilder.java
@@ -1,8 +1,8 @@
 package com.bloomberg.server.arithmeticservice.businesslogic.interfaces;
 
-import com.bloomberg.server.arithmeticservice.configuration.OperationsConfig;
+import com.bloomberg.server.arithmeticservice.businesslogic.exceptions.ExpressionBuildException;
 import com.bloomberg.server.arithmeticservice.models.Expression;
 
 public interface IExpressionBuilder {
-    String build(Expression expression, OperationsConfig operationsConfig);
+    String build(Expression expression) throws ExpressionBuildException;
 }
